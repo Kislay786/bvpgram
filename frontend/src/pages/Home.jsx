c1382c398d4b21e326865d68e50da7def01e0cce
Kislay Thakur <kislaythakur007@gmail.com>
2024-09-01 21:06:55 +0530

    Home login n register page UI made successfully

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import {Link} from "react-router-dom"
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa"
+
+function Home() {
+  return (
+    <>
+        <section className="heading">
+          <h1> B.V.P. Grievance Redressal and Management </h1>
+          <p>Please choose from an option below</p>
+        </section>
+
+        <Link to='/new-ticket' className="btn btn-reverse btn-block">
+          <FaQuestionCircle /> Create New Grievance Ticket
+        </Link>
+
+        <Link to='/tickets' className="btn btn-block">
+          <FaTicketAlt /> View My Grievance Tickets
+        </Link>
+    </>
+  )
+}
+export default Home
